27d39fd967074adec36ba14ff86674d193279c07
Julián Castro <juliancastro18k@gmail.com>
2021-06-09 02:09:43 -0300

    Arreglos de responsive, actualiza doc, etc

--- src/main/resources/static/js/permiso/html2pdf.js
+++ src/main/resources/static/js/permiso/html2pdf.js
@@ -1,11 +1,15 @@
 function generarPDF() {
 
-	const element = document.getElementById("datosPermiso");
+	const element = document.getElementById("datosPermiso")
+	const nombre = document.getElementById("nombre").innerText.toLowerCase()
+	const apellido = document.getElementById("apellido").innerText.toLowerCase()
+	const exp = /\d{2}\/\d{2}\/\d{2}/
+	const fecha = document.getElementById("fecha").innerText.match(exp)[0].replaceAll("/", "-")
 	
 	html2pdf()
 	.set({
 		margin: 1,
-		filename: 'permiso.pdf',
+		filename: 'permiso_' + nombre + '_' + apellido + '_' + fecha + '.pdf',
 		image: {
 			type: 'jpeg',
 			quality: 0.98
